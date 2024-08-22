e178cfa82e20f65e272ef0893fb422a93a12bbe4
Rintaro Okamura <rintaro.okamura@gmail.com>
2024-08-22 18:18:09 +0900

    feat: use ast-grep as lint engine (#2)
    - replace placeholders in action.yml
    - add sgconfig.yml and teatdata
    - add to-rdjsonl.jq and fix script.sh
    
    Signed-off-by: Rintaro Okamura <rintaro.okamura@gmail.com>

--- script.sh
+++ script.sh
@@ -5,6 +5,10 @@ if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
 fi
 
+if [ -z "${INPUT_SG_VERSION}" ]; then
+  INPUT_SG_VERSION="${DEFAULT_SG_VERSION}"
+fi
+
 mkdir -p "${INPUT_OUTPUT_DIR}"
 OUTPUT_FILE_NAME="reviewdog-${INPUT_TOOL_NAME}"
 if [[ "${INPUT_REPORTER}" == "sarif" ]]; then
@@ -13,16 +17,27 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-echo '::group::🐶 Installing misspell ... https://github.com/client9/misspell'
+echo '::group::🐶 Installing ast-grep ... https://github.com/ast-grep/ast-grep'
 TEMP_PATH="$(mktemp -d)"
 PATH="${TEMP_PATH}:$PATH"
-wget -O - -q https://git.io/misspell | sh -s -- -b "${TEMP_PATH}"
+
+curl --silent --show-error --fail \
+  --location "https://github.com/ast-grep/ast-grep/releases/download/${INPUT_SG_VERSION}/app-x86_64-unknown-linux-gnu.zip" \
+  --output "${TEMP_PATH}/sg.zip"
+
+unzip -u "${TEMP_PATH}/sg.zip" -d "${TEMP_PATH}/temp-sg"
+install "${TEMP_PATH}/temp-sg/ast-grep" "${TEMP_PATH}"
+rm -rf "${TEMP_PATH}/sg.zip" "${TEMP_PATH}/temp-sg"
+
 echo '::endgroup::'
 
-echo '::group:: Running misspell with reviewdog 🐶 ...'
+echo '::group:: Running ast-grep with reviewdog 🐶 ...'
+
 # shellcheck disable=SC2086
-misspell -locale="${INPUT_LOCALE}" . |
-  reviewdog -efm="%f:%l:%c: %m" \
+ast-grep scan --config="${INPUT_SG_CONFIG}" --json=compact |
+  jq -f "to-rdjsonl.jq" -c |
+  reviewdog \
+    -f=rdjsonl \
     -name="${INPUT_TOOL_NAME}" \
     -reporter="${INPUT_REPORTER}" \
     -filter-mode="${INPUT_FILTER_MODE}" \
---
+++ testdata/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	_ = context.TODO()
+
+	fmt.Println("hello world")
+}
+
+// Deprecated: deprecated function
+func New() {
+	// FIXME: implement me
+	fmt.Println("new function")
+}
